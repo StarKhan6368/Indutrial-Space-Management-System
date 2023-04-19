ca241501c464045c837551131fd359b242b545bd
Star Khan <starkhan6368@gmail.com>
2023-04-19 18:04:35 +0530

    Small Fixes

--- ISMS/routes.py
+++ ISMS/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for, flash, get_flashed_messages
 from ISMS import app
 from ISMS.forms import LoginForm, RegisterForm
-from ISMS.database import User
+from ISMS.models import User
 
 @app.route("/login", methods=["GET","POST"])
 def login():
