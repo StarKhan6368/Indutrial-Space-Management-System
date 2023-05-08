a1153ea9c43326174c62c17e870b5175f4de584e
Star Khan <starkhan6368@gmail.com>
2023-05-09 01:28:42 +0530

    Miscallaneous Fixes

--- run.py
+++ run.py
@@ -1,17 +1,17 @@
 from ISMS import app
-# from mqtt_to_postgres import process_me
-# import multiprocessing
+import multiprocessing
 
-# def Flask_run():
-#     app.run(debug=True)
-    
-# if __name__ == "__main__":
-#     mqtt_to_postgres = multiprocessing.Process(target=process_me)
-#     flask_app = multiprocessing.Process(target=Flask_run)
-#     flask_app.start()
-#     mqtt_to_postgres.start()
-#     flask_app.join()
-#     mqtt_to_postgres.join()
-
-if __name__ == "__main__":
+def Flask_run():
     app.run(debug=True)
+    
+if __name__ == "__main__":
+    mqtt_to_postgres = multiprocessing.Process(target=mqtt_start)
+    flask_app = multiprocessing.Process(target=Flask_run)
+    flask_app.start()
+    mqtt_to_postgres.start()
+    flask_app.join()
+    mqtt_to_postgres.join()
+    print("App Has Been Terminated")
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
