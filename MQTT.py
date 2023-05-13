e94caa715cfec4ee7e74d26ae2b8c5a6a1fca5b6
Star Khan <starkhan6368@gmail.com>
2023-05-13 14:26:18 +0530

    fixed errors with mqtt

--- MQTT.py
+++ MQTT.py
@@ -1,14 +1,15 @@
 from ISMS.models import Sensor, Cluster, Employee
 from paho.mqtt import client as mqtt_client
 from ISMS import app, db
-# from camera import Camera
+from camera import Camera
 import json
 
 BROKER = '192.168.0.104'
 CAM_API = "http://1291.168.0.123"
 PORT = 1883
-ESP_ENV_TOPIC = "esp32"
-ESP_CAM_TOPIC = "esp32cam"
+ESP_ENV_TOPIC = "ENV_DATA"
+ESP_CAM_TOPIC = "RFID_DATA"
+FACE_RECON_TOPIC = "FACE_RECON"
 USERNAME = "<mqtt_username>"
 PASSWORD = "<mqtt_password>"
 CLIENT_ID = "Flask-App"
@@ -49,24 +50,23 @@ def env_subscribe(client: mqtt_client):
     
 
 def cam_subscribe(client: mqtt_client):
-    # camera = Camera(CAM_API)
+    camera = Camera(CAM_API)
     def on_message(client, userdata, msg):
         payload = msg.payload.decode("utf-8")
         data = json.loads(payload)
         print(data, end="\n<------------->\n")
         if "rfid_detected" in data:
-            # with app.app_context():
-            #     employee = Employee.query.filter_by(emp_id = data["rfid"]).first()
-            pass
+            with app.app_context():
+                employee = Employee.query.filter_by(emp_id = data["rfid"]).first()
+                if employee:
+                    print(employee)
     client.subscribe(ESP_CAM_TOPIC)
     client.on_message = on_message
-                
-                
 
 def mqtt_start():
     print("Connecting to MQTT Server")
     client = connect_mqtt()
-    #env_subscribe(client)
+    env_subscribe(client)
     cam_subscribe(client)
     client.loop_forever()
     
